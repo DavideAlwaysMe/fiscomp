e9aaa7a3bd0302ff1629aad96147983ed6589191
DavideAlwaysMe <davidededededede@gmail.com>
2021-10-26 23:34:17 +0200

    predizione correzione

--- oscillatore.c
+++ oscillatore.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#define ALGORITMI_NUM 6
+#define ALGORITMI_NUM 7
 
 struct valori{
     double x;
@@ -14,6 +14,7 @@ struct valori puntocentrale(double dt, double omegaquadro, struct valori valori_
 struct valori mezzopasso(double dt, double omegaquadro, struct valori valori_n);
 struct valori verlet(double dt, double omegaquadro, struct valori valori_n, double x_old);
 struct valori verletautosufficiente(double dt, double omegaquadro, struct valori valori_n);
+struct valori predcorr(double dt, double omegaquadro, struct valori valori_n, double x_old);
 //da fare quando aggiungo algoritmo: creare funzione, cambiare ALGORITMI_NUM e aggiungere il nome a nomi[] e aggiungere in errore
 
 //void trovaperiodo()
@@ -24,7 +25,7 @@ int main(int argc, char* argv[]) {
 
     if(argc!=8 || atof(argv[7])<0 || atof(argv[7])>ALGORITMI_NUM){
         //Il programma eseguira tutti gli algoritmi e salverà i dati in %d file del tipo Eulero.dat.
-        fprintf(stderr,"Per l\'esecuzione del programma è necessario passare come argomenti: x0, v0, dt, T, k, m, algoritmo.\nAlgoritmo è un numero intero, scegliere tra:\n0 Eulero\n1 Eulero-Cromer\n2 Punto centrale\n3 Mezzo passo\n4 Verlet\n5 Verlet autosufficiente\n");
+        fprintf(stderr,"Per l\'esecuzione del programma è necessario passare come argomenti: x0, v0, dt, T, k, m, algoritmo.\nAlgoritmo è un numero intero, scegliere tra:\n0 Eulero\n1 Eulero-Cromer\n2 Punto centrale\n3 Mezzo passo\n4 Verlet\n5 Verlet autosufficiente\n6 Predizione Correzione\n");
         exit(1);
     }
 
@@ -36,7 +37,7 @@ int main(int argc, char* argv[]) {
     FILE *fptr;
     //t x e v sono array dinamici nei quali vengono salvati i valori di t x e v
     double *t,*x,*v;
-    char  nomi[][50]={"Eulero", "Eulero-Cromer", "Punto_Centrale", "Mezzo_Passo","Verlet","Verlet_autosufficiente"}, nomefile[100];
+    char  nomi[][50]={"Eulero", "Eulero-Cromer", "Punto_Centrale", "Mezzo_Passo","Verlet","Verlet_autosufficiente","Predizione_correzione"}, nomefile[100];
 
     //assegno i parametri di esecuzione alle variabili iniziali
     valori_n.x=atof(argv[1]);
@@ -76,27 +77,15 @@ int main(int argc, char* argv[]) {
     v[0]=valori_n.v;
     t[0]=0.;
 
-    if(algoritmo!=4){
-        //per i primi quattro algoritmi è sufficiente questo ciclo for che fa uso di un array di puntatori a funzione
-        for(i=1;i<=npassi;i++){
-            //calcolo i valori con la funzione prelevata dall'array algoritmi_lista
-            valori_n=(*algoritmo_lista[algoritmo])(dt,omegaquadro,valori_n);
-            energia_n=energia(m,valori_n.v,k,valori_n.x); //calcolo energia utilizzando la rispettiva funzione energia
-            energia_rapporto=(energia_n-energia_0)/energia_0;
-            tempo=(double)i*dt; //calcolo il tempo così per non perdere precisione per via dell'approssimazione di numeri molto piccoli
-            fprintf(fptr,"%g %g %g %g %g\n",tempo,valori_n.x,valori_n.v,energia_n,energia_rapporto); //stampo su un file t, x, v e energia
-            x[i]=valori_n.x;
-            v[i]=valori_n.v;
-            t[i]=tempo;
-        }
-    }else if(algoritmo==4){
-        //poiché verlet necessità di un ulteriore parametro come argomento rispetto ai precedenti algoritmi non può essere inserito nell'array
+    //questo if permette di selezionare l'algoritmo, alcuni algoritmi sono descritti in else, messi tutti insieme per rendere più breve il codice
+    //dal momento che le rispettive funzioni avevano gli stessi argomenti e potevano essere inseriti in un array di funzioni
+    if(algoritmo==4){
+        //poiché verlet necessita di un parametro in più (x_old) non può essere inserito nell'array
         valori_n=eulero(dt,omegaquadro,valori_n); //conterrà i valori di x e v al primo passo, necessario per verlet non autosufficiente
         x[1]=valori_n.x;
 
         //per calcolare la velocità al passo 1 uso verlet ma non posso usare la funzione
         x[2]=2*x[1]-x[0]-omegaquadro*x[1]*dt*dt;
-        printf("%lf",x[2]);
         v[1]=(x[2]-x[0])/(2*dt);
         energia_n=energia(m,v[1],k,x[1]); //calcolo energia utilizzando la rispettiva funzione energia
         energia_rapporto=(energia_n-energia_0)/energia_0;
@@ -113,8 +102,39 @@ int main(int argc, char* argv[]) {
             v[i]=valori_n.v;
             t[i]=tempo;
         }
-    }else if(algoritmo==5){
+    }else if(algoritmo==6){
+        valori_n=eulero(dt,omegaquadro,valori_n); //conterrà i valori di x e v al primo passo calcolati con eulero
+        x[1]=valori_n.x;
+        v[1]=valori_n.v;
 
+        energia_n=energia(m,v[1],k,x[1]); //calcolo energia utilizzando la rispettiva funzione energia
+        energia_rapporto=(energia_n-energia_0)/energia_0;
+        fprintf(fptr,"%g %g %g %g %g\n",dt,x[1],v[1],energia_n,energia_rapporto); //stampo su un file t, x, v e energia
+
+        for(i=2;i<=npassi;i++){
+            //calcolo i valori con la funzione prelevata dall'array algoritmi_lista
+            valori_n=predcorr(dt,omegaquadro,valori_n,x[i-2]);
+            energia_n=energia(m,valori_n.v,k,valori_n.x); //calcolo energia utilizzando la rispettiva funzione energia
+            energia_rapporto=(energia_n-energia_0)/energia_0;
+            tempo=(double)i*dt; //calcolo il tempo così per non perdere precisione per via dell'approssimazione di numeri molto piccoli
+            fprintf(fptr,"%g %g %g %g %g\n",tempo,valori_n.x,valori_n.v,energia_n,energia_rapporto); //stampo su un file t, x, v e energia
+            x[i]=valori_n.x; //i-1 perché i parte da 1 e non da 0
+            v[i]=valori_n.v;
+            t[i]=tempo;
+        }
+    } else {
+        //per gli algoritmi 0 1 2 3 5 è sufficiente questo ciclo for che fa uso di un array di puntatori a funzione
+        for(i=1;i<=npassi;i++){
+            //calcolo i valori con la funzione prelevata dall'array algoritmi_lista
+            valori_n=(*algoritmo_lista[algoritmo])(dt,omegaquadro,valori_n);
+            energia_n=energia(m,valori_n.v,k,valori_n.x); //calcolo energia utilizzando la rispettiva funzione energia
+            energia_rapporto=(energia_n-energia_0)/energia_0;
+            tempo=(double)i*dt; //calcolo il tempo così per non perdere precisione per via dell'approssimazione di numeri molto piccoli
+            fprintf(fptr,"%g %g %g %g %g\n",tempo,valori_n.x,valori_n.v,energia_n,energia_rapporto); //stampo su un file t, x, v e energia
+            x[i]=valori_n.x;
+            v[i]=valori_n.v;
+            t[i]=tempo;
+        }
     }
     fclose(fptr);
     return(0);
@@ -187,7 +207,19 @@ struct valori verletautosufficiente(double dt, double omegaquadro, struct valori
     return valori_new;
 }
 
+struct valori predcorr(double dt, double omegaquadro, struct valori valori_n, double x_old){
+    struct valori valori_new;
+    double x_futuro,phi_new,phi_n;
 
+    //ho bisogno di x_old per calcolare un x(n+1) approssimativo per calcolare poi phi(n+1)
+    x_futuro=x_old+2*valori_n.v*dt;
+    phi_new=-omegaquadro*x_futuro;
+    phi_n=-omegaquadro*valori_n.x;
+    
+    valori_new.v=valori_n.v+((phi_new+phi_n)/2.)*dt;
+    valori_new.x=valori_n.x+((valori_n.v+valori_new.v)/2.)*dt;
+    return valori_new;
+}
 
 double energia(double m,double v,double k,double x){
     double e_potenziale,e_cinetica;
--- oscillatore.py
+++ oscillatore.py
@@ -1,27 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-t,x,v,e,delta_E=np.loadtxt('Verlet_autosufficiente_dt0.05.dat',unpack=True,usecols=(0,1,2,3,4))
+t, x, v, e, delta_E = np.loadtxt(
+    'Predizione_correzione_dt0.05.dat', unpack=True, usecols=(0, 1, 2, 3, 4))
 
-#tempo e coordinata x
-plt.plot(t,x, color='g')
+# tempo e coordinata x
+plt.plot(t, x, color='g')
 plt.title('Algoritmo')
 plt.xlabel('t')
 plt.ylabel('x')
-#plt.figure(figsize=(7,7))
+# plt.figure(figsize=(7,7))
 plt.savefig('txx.png', dpi=199)
 plt.clf()
 
-#tempo e coordinata v
-plt.plot(t,v, color='g')
+# tempo e coordinata v
+plt.plot(t, v, color='g')
 plt.title('Algoritmo')
 plt.xlabel('t')
 plt.ylabel('v')
 plt.savefig('txv.png', dpi=199)
 plt.clf()
 
-#x e v
-plt.plot(x,v, color='g')
+# x e v
+plt.plot(x, v, color='g')
 plt.title('Algoritmo')
 plt.xlabel('x')
 plt.ylabel('v')
